b3ee23e7f55d8e05fd738c31b38d9f386a523ea4
Sergey Konovalov <servako@gmail.com>
2016-03-27 14:40:17 +0300

    commit WorkWithExcelWorkbook

---
+++ WindowsFormsApplicationExcelWorkbook/WindowsFormsApplicationExcelWorkbook/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace WindowsFormsApplicationExcelWorkbook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.bChooseFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Title = "Open File";
+            // 
+            // bChooseFile
+            // 
+            this.bChooseFile.Location = new System.Drawing.Point(12, 12);
+            this.bChooseFile.Name = "bChooseFile";
+            this.bChooseFile.Size = new System.Drawing.Size(142, 33);
+            this.bChooseFile.TabIndex = 0;
+            this.bChooseFile.Text = "Select Excel file";
+            this.bChooseFile.UseVisualStyleBackColor = true;
+            this.bChooseFile.Click += new System.EventHandler(this.bChooseFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 120);
+            this.Controls.Add(this.bChooseFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button bChooseFile;
+    }
+}
+
---
+++ WindowsFormsApplicationExcelWorkbook/WindowsFormsApplicationExcelWorkbook/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace WindowsFormsApplicationExcelWorkbook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "xlsx files (*.xlsx)|*.xlsx";
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (var document = SpreadsheetDocument.Open(openFileDialog1.FileName, true))
+                    {
+                        var workbookPart = document.WorkbookPart;
+                        var workbook = workbookPart.Workbook;
+
+                        var sheets = workbook.Descendants<Sheet>();
+                        foreach (var sheet in sheets)
+                        {
+                            var worksheetPart = (WorksheetPart)workbookPart.GetPartById(sheet.Id);
+                            var sharedStringPart = workbookPart.SharedStringTablePart;
+                            var values = sharedStringPart.SharedStringTable.Elements<SharedStringItem>().ToArray();
+
+                            var cells = worksheetPart.Worksheet.Descendants<Cell>();
+                            foreach (var cell in cells)
+                            {
+                                string value = String.Empty;
+                                if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
+                                {
+                                    var index = int.Parse(cell.CellValue.Text);
+                                    value = values[index].InnerText;
+                                }
+                                else
+                                {
+                                    value = cell.CellValue.Text;
+                                }
+
+                                if (value.ToLower() == "red")
+                                {
+                                    CellFormat cellFormat = cell.StyleIndex != null ? GetCellFormat(workbookPart, cell.StyleIndex).CloneNode(true) as CellFormat : new CellFormat();
+                                    cellFormat.FillId = InsertFill(workbookPart, GenerateFill());
+                                    cell.StyleIndex = InsertCellFormat(workbookPart, cellFormat);
+                                }
+                            }
+                        }
+
+                        workbook.Save();
+
+                        MessageBox.Show("Done!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+        }
+
+        public CellFormat GetCellFormat(WorkbookPart workbookPart, uint styleIndex)
+        {
+            return workbookPart.WorkbookStylesPart.Stylesheet.Elements<CellFormats>().First().Elements<CellFormat>().ElementAt((int)styleIndex);
+        }
+
+        public uint InsertFill(WorkbookPart workbookPart, Fill fill)
+        {
+            Fills fills = workbookPart.WorkbookStylesPart.Stylesheet.Elements<Fills>().First();
+            fills.Append(fill);
+            return (uint)fills.Count++;
+        }
+
+        public Fill GenerateFill()
+        {
+            Fill fill = new Fill();
+
+            PatternFill patternFill = new PatternFill() { PatternType = PatternValues.Solid };
+            ForegroundColor foregroundColor1 = new ForegroundColor() { Rgb = "FFF00000" };
+
+            patternFill.Append(foregroundColor1);
+
+            fill.Append(patternFill);
+
+            return fill;
+        }
+
+        public uint InsertCellFormat(WorkbookPart workbookPart, CellFormat cellFormat)
+        {
+            CellFormats cellFormats = workbookPart.WorkbookStylesPart.Stylesheet.Elements<CellFormats>().First();
+            cellFormats.Append(cellFormat);
+            return (uint)cellFormats.Count++;
+        }
+    }
+}
