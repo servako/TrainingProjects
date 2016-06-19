445120cb6fbff672f14e78328a328b7c6448f4c4
Sergey Konovalov <servako@gmail.com>
2016-06-19 20:12:05 +0300

    CallerInfo

---
+++ Caller-Info_Attributes_Code/CallerInfoAttbClient/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CallerInfoAttbLib;
+
+namespace CallerInfoAttbClient
+{
+    public partial class Form1 : Form
+    {
+        Employee emp = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            emp = new Employee();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            emp.PropertyChanged += emp_PropertyChanged;
+
+            emp.EmployeeID = int.Parse(textBox1.Text);
+            emp.FirstName = textBox2.Text;
+            emp.LastName = textBox3.Text;
+
+            MessageBox.Show(emp.AddEmployee());
+            
+        }
+
+        void emp_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            MessageBox.Show("Property " + e.PropertyName + " has changed!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Caller-Info_Attributes_Code/CallerInfoAttbLib/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System.Runtime.CompilerServices;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace CallerInfoAttbLib
+{
+    public class Employee:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Employee([CallerMemberName]string sourceMemberName = "", [CallerFilePath]string sourceFilePath = "", [CallerLineNumber]int sourceLineNo = 0)
+        {
+            Debug.WriteLine("Member Name : " + sourceMemberName);
+            Debug.WriteLine("File Name : " + sourceFilePath);
+            Debug.WriteLine("Line No. : " + sourceLineNo);
+        }
+
+        private int intEmployeeID;
+        public int EmployeeID
+        {
+            get
+            {
+                return intEmployeeID;
+            }
+            set
+            {
+                SetPropertyValue<int>(ref intEmployeeID, value);
+            }
+        }
+
+        private string strFirstName;
+        public string FirstName
+        {
+            get 
+            {
+                return strFirstName;
+            }
+            set 
+            {
+                SetPropertyValue<string>(ref strFirstName, value);
+            }
+        }
+
+        private string strLastName;
+        public string LastName
+        {
+            get
+            {
+                return strLastName;
+            }
+            set
+            {
+                SetPropertyValue<string>(ref strLastName, value);
+            }
+        }
+
+        protected bool SetPropertyValue<T>(ref T varName, T propValue, [CallerMemberName] string propName = null)
+        {
+            varName = propValue;
+            if (PropertyChanged != null)
+            {
+                PropertyChangedEventArgs evt = new PropertyChangedEventArgs(propName);
+                this.PropertyChanged(this, evt);
+                Debug.WriteLine("Member Name : " + propName);
+            }
+            return true;
+        }
+
+        public string AddEmployee([CallerMemberName]string sourceMemberName="",[CallerFilePath]string sourceFilePath="",[CallerLineNumber]int sourceLineNo=0)
+        {
+            Debug.WriteLine("Member Name : " + sourceMemberName);
+            Debug.WriteLine("File Name : " + sourceFilePath);
+            Debug.WriteLine("Line No. : " + sourceLineNo);
+            //do database INSERT here
+            return "Employee added successfully!"; 
+        }
+    
+    }
+}
